e2723fc2b04046234947966865ce3cd430da253e
mihailmihail <vandaniel@mail.ru>
2024-02-06 17:32:30 +0300

    vectorN

--- game-server/src/main/java/ru/sap/spaceBattle/server/Vector.java
+++ game-server/src/main/java/ru/sap/spaceBattle/server/Vector.java
@@ -1,18 +1,5 @@
 package ru.sap.spaceBattle.server;
 
-import lombok.Getter;
-
-@Getter
-public class Vector {
-    private final int x;
-    private final int y;
-
-    public Vector(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Vector plus(Vector next) {
-        return new Vector(x + next.getX(), y + next.getY());
-    }
+public interface Vector {
+    Vector plus(Vector next);
 }
